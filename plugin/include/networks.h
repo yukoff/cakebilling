cd82d7253fd9e67a2b8eda804cb4c08737d665af
norguhtar <norguhtar@30409bb7-0425-0410-9404-f146f342b1ef>
2007-08-21 17:58:29 +0000

    добавлена первая реализация списка сетей

--- plugin/include/networks.h
+++ plugin/include/networks.h
@@ -21,4 +21,19 @@ struct network_item {
 	struct network_item *next; //!< указатель на следующий элемент
 };
 
+//! Создает новый список сетей. В случае неудачи возвращает NULL
+struct networks_list *create_networks_list();
+
+//! Добавляет новый элемент в список сетей, Если after_item равен NULL, то элемент добавляется в начало. В противном случае элемент добавляется после after_item. При ошибке возвращает NULL.  
+struct network_item *add_item_networks_list(struct networks_list *networks_list, struct network_item *after_item);
+
+//! Удаляет элемент item из списка сетей. В случае если элемент равен NULL удаляется последний элемент. Возвращает количество оставшихся элементов или -1 в случае ошибки.
+int del_item_networks_list(struct networks_list *networks_list, struct network_item *item);
+
+//! Удаляет список сетей. Возвращает -1 в случае ошибки, в случае удачного завершения возвращает 0;
+int destroy_networks_list(struct networks_list *networks_list);
+
+//! Осуществляет поиск направления совпадающего с указаным ip. В случае удачного поиска возвращает направление, в портивоположном случае возвращает NULL.
+struct traffic_item *search_destination(struct networks_list *networks_list, struct in_addr ip);
+
 #endif /*NETWORKS_H_*/
