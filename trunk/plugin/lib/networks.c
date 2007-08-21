e72f4dfe1265d20de99a9b84813ee0b58a9515ad
norguhtar <norguhtar@30409bb7-0425-0410-9404-f146f342b1ef>
2007-08-21 17:58:29 +0000

    добавлена первая реализация списка сетей

--- trunk/plugin/lib/networks.c
+++ trunk/plugin/lib/networks.c
@@ -34,26 +34,84 @@ struct network_item *add_item_networks_list(struct networks_list *networks_list,
 		networks_list->first = new_network_item;
 		networks_list->last = new_network_item;
 		networks_list->curr = new_network_item;
-		networks_list->count++;
 	}
 	else {
 		if (after_item == NULL) {
 			new_network_item->next=networks_list->first;
 			networks_list->first->prev = new_network_item;
 			networks_list->first = new_network_item;
-			networks_list->count++;
 		}
 		else {
 			new_network_item->next = after_item->next;
 			new_network_item->prev = after_item;
 			after_item->next = new_network_item;
-			networks_list->count++;
 		}
 	}
 	
+	networks_list->count++;
 	return new_network_item;
 }
 
-struct network_item *del_item_networks_list(struct networks_list *networks_list, struct network_item *item) {
+int del_item_networks_list(struct networks_list *networks_list, struct network_item *item) {
+	struct network_item *del_item;
+
+	if (networks_list == NULL) return -1;
 	
+	if (item == NULL) {
+		if (networks_list->last != NULL) {
+			del_item = networks_list->last;
+			networks_list->last = del_item->prev;
+			networks_list->last->next = NULL;
+			free(del_item);
+			networks_list->count--;
+		} 
+		else {
+			return -1;
+		}
+	} 
+	else {
+		if (item->prev != NULL) {
+			item->prev->next = item->next;
+		}
+		else {
+			networks_list->first = item->next;
+		}
+		if (item->next != NULL) {
+			item->next->prev = item->prev;
+		}
+		else {
+			networks_list->last = item->prev;
+		}
+		if (networks_list->curr == item) {
+			networks_list->curr == item->prev;
+		}
+		free(item);
+		networks_list->count--;
+	}
+
+	return networks_list->count;
+}
+
+int destroy_networks_list(struct networks_list *networks_list) {
+	if (networks_list == NULL) return -1;
+	
+	while (networks_list->count > 0) {
+		del_item_networks_list(networks_list,NULL);
+	}
+	free(networks_list);
+	return 0;
+}
+
+struct traffic_item *search_destination(struct networks_list *networks_list, struct in_addr ip) {
+	if (networks_list == NULL) return NULL;
+	
+	networks_list->curr = networks_list->first;
+	while (networks_list->curr != NULL) {
+		if ((ip.s_addr & networks_list->curr->netmask.s_addr) == networks_list->curr->network.s_addr) {
+			return networks_list->curr->destination;
+		}
+		networks_list->curr = networks_list->curr->next;
+	}
+	
+	return NULL;
 }
