da292809585fb0cc9c4d7a5c3a856d49ee1afe8d
norguhtar <norguhtar@30409bb7-0425-0410-9404-f146f342b1ef>
2008-11-07 12:39:33 +0000

    Create aggregate function

--- SquidLogger/squidlogger.sql
+++ SquidLogger/squidlogger.sql
@@ -1,16 +1,11 @@
-
-drop table url;
-drop table traffic;
-drop table proxy_server;
-drop table proxy_user_ip;
-drop table proxy_user;
-
 create table proxy_user (
 	id		int not null auto_increment,
 	name		varchar(255) not null,
 	passwd		varchar(255),
 	primary key(id)
 );
+
+insert into proxy_user (id,name) values(1,'test');
 
 create table proxy_user_ip (
 	id			int not null auto_increment,
@@ -20,6 +15,8 @@ create table proxy_user_ip (
 	foreign key(proxy_user_id) references proxy_user(id)
 );
 
+insert into proxy_user_ip (id,proxy_user_id,ip) values(1,1,'127.0.0.1');
+
 create table proxy_server (
 	id		int not null auto_increment,
 	ip		varchar(25) not null,
@@ -28,16 +25,18 @@ create table proxy_server (
 	primary key(id)
 );
 
+insert into proxy_server (id,ip,name) values(1,'127.0.0.1','test proxy');
+
 create table traffic (
 	id			int not null auto_increment,
 	proxy_user_ip_id	int not null,
 	proxy_server_id		int not null,
 	starttime		timestamp,
 	period			int,
-	inbytes			int(32),
-	outbytes		int(32),
-	cinbytes		int(32),
-	coutbytes		int(32),
+	inbytes			int(32) DEFAULT 0,
+	outbytes		int(32) DEFAULT 0,
+	cinbytes		int(32) DEFAULT 0,
+	coutbytes		int(32) DEFAULT 0,
 	primary key(id),
 	foreign key(proxy_user_ip_id) references proxy_user_ip(id),
 	foreign key(proxy_server_id) references proxy_server(id)
@@ -50,8 +49,8 @@ create table url (
 	host			varchar(255),
 	url			varchar(2048),
 	cached			bool,
-	inbytes			int(32),
-	outbytes		int(32),
+	inbytes			int(32) DEFAULT 0,
+	outbytes		int(32) DEFAULT 0,
 	primary key(id),
 	foreign key(traffic_id) references traffic(id)
 );
