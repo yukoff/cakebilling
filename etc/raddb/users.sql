684ba66a286f9ea87c7ed0cac6fbedf6495fc2b5
Yury Buldakov <yury.buldakov@gmail.com>
2016-03-07 04:28:16 +0200

    Import etc samples

---
+++ etc/raddb/users.sql
@@ -0,0 +1,18 @@
+# необходимо для авторизации через SQL
+DEFAULT         Auth-Type:=Local
+
+# тестовый пользователь. Если будете тестировать с pppd и mschap авторизацией пропишите
+# Auth-Type:=MS-CHAP вместо Auth-Type:=Local, иначе RADIUS сервер не авторизует клиента.
+# Только для тестов. 
+#test            Auth-Type:=Local, User-Password == "test"
+##test            Auth-Type:=MS-CHAP, User-Password == "test"
+#                Service-Type = Framed-User,
+#                Framed-Protocol = PPP,
+#                Framed-IP-Address = 192.168.0.200,
+#                Framed-IP-Netmask = 255.255.255.0,
+#                Framed-Route = "192.168.1.0/24 192.168.200.204/32 1",
+#                Reply-Message = "Just Test",
+#                Acct-Interim-Interval = 60,
+#                Session-Timeout = 120,
+#		 Framed-Routing = Broadcast-Listen,
+#                Framed-Compression = None
